9cf583a7fb7a5d95570e876ad642d93d63a6876c
Marcos Valderrabano <ing.marco.valu@gmail.com>
2022-05-16 10:37:43 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 // Add your code here
-function submitData() {
+document.getElementById('user-form').addEventListener('submit', (event) => {
+  event.preventDefault();
+  console.log(event.target.username.value);
+  console.log(event.target.email.value);
+  submitData(event.target.username.value, event.target.email.value)
+});
+
+function submitData(name, email) {
+  const userObject = { 
+    name: name, 
+    email: email
+  }
   const configurationObject = {
     method: "POST",
     headers: {
       "Content-type": "application/json",
       Accept: "application/json"
     },
-    body: JSON.stringify({
-      name: 'Steve',
-      email: 'steve@steve.com'
-    })
+    body: JSON.stringify(userObject)
   };
   return fetch("http://localhost:3000/users", configurationObject)
     .then((response) => response.json())
-    .then((userData) => console.log(userData))
-    .catch((error) => console.log(error.message));
+    .then((userData) => {
+      console.log(userData)
+      const body = document.getElementById('main');
+      const h3 = document.createElement('h3');
+      h3.innerHTML = `Id: ${userData.id}`;
+
+      body.appendChild(h3);
+    })
+    .catch(() => {
+      const p = document.createElement('p');
+      p.innerHTML = "Unauthorized Access";
+
+      document.body.appendChild(p);
+    });
 }
 
-submitData();
+//submitData();
