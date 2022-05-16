8e7a1e79109830cf6d3838b07b6f4c2e69f88bb9
Marcos Valderrabano <ing.marco.valu@gmail.com>
2022-05-15 21:16:21 -0700

    work in progress

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+function submitData() {
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      name: 'Steve',
+      email: 'steve@steve.com'
+    })
+  };
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then((response) => response.json())
+    .then((userData) => console.log(userData))
+    .catch((error) => console.log(error.message));
+}
+
+submitData();
